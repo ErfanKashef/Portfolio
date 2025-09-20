09ba86c466fc5b456e308ae2b67668103eb0df69
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-09-20 15:48:01 +0330

    change:bg side bar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -70,7 +70,7 @@ const Navbar = () => {
 
       {/* Sidebar */}
       <div
-        className={`fixed top-0 right-0 h-full w-80 bg-gray-700 shadow-2xl transform transition-transform duration-300 ease-in-out z-50 md:hidden ${
+        className={`fixed top-0 right-0 h-full w-80 bg-background shadow-2xl transform transition-transform duration-300 ease-in-out z-50 md:hidden ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
